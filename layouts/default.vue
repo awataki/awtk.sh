f268781a81b06d350d78422f574a92e3cf48023c
awataki <awataki1206@gmail.com>
2020-08-24 21:41:20 +0900

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      app
+      width="220px"
+      overlay-color="primary"
+      :mini-variant="!mini"
+      :expand-on-hover="!mini"
+    >
+      <v-list class="py-0">
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.link"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      app
+      clipped-left
+      fixed
+      flat
+      color="primary"
+      class="elevation-1"
+      dark
+    >
+      <v-app-bar-nav-icon v-model="drawer" @click="drawer = !drawer" />
+      <h1 class="px-6">
+        awtk.sh
+      </h1>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: undefined,
+      items: [
+        {
+          title: 'Profile',
+          link: '/',
+          icon: 'mdi-account'
+        },
+        {
+          title: 'Services',
+          link: '/services',
+          icon: 'mdi-application'
+        }
+      ]
+    }
+  },
+  computed: {
+    mini () {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs': return true
+        case 'sm': return true
+        case 'md': return true
+        case 'lg': return false
+        case 'xl': return false
+      }
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" md="11">
+      <v-card>
+        <v-card-title>
+          <h3>
+            Yuga Iwata
+          </h3>
+        </v-card-title>
+        <v-card-subtitle>
+          <h4>
+            a.k.a awataki
+          </h4>
+        </v-card-subtitle>
+        <v-card-text>
+          <v-timeline dense>
+            <Skills />
+            <Hobbies />
+            <Links />
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Vue from 'vue'
+import Skills from '~/components/Skills.vue'
+import Hobbies from '~/components/Hobbies.vue'
+import Links from '~/components/Links'
+
+export default Vue.extend({
+  components: {
+    Skills,
+    Hobbies,
+    Links
+  }
+})
+</script>
