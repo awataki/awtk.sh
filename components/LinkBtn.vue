40cf003ef2737f7003e3928e5ef115c02aa786ca
awataki <awataki1206@gmail.com>
2020-08-24 21:42:36 +0900

    add link btn component

---
+++ components/LinkBtn.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-btn depressed color="white" :href="href" class="text-capitalize">
+    <slot />
+    <v-icon>mdi-open-in-new</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
